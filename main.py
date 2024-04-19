af14f08e3388f1d36a2261669028b30d724db93a
AdrianSoh <128157427+SohWeeChen@users.noreply.github.com>
2024-04-19 21:32:04 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import ultralytics,os,cv2,json
+
+model = ultralytics.YOLO('yolov5s.pt')
+
+for i in os.listdir('images/'):
+    img = cv2.imread(f'images/{i}')
+    #model.predict(i,save_text=True)
+    result = model(img)
+    r = json.loads(result[0].tojson())
+    name = r[0]['name']
+    
